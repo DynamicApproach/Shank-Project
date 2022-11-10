40b5c6a300a27d0ca7db4611cbd64ade6f77dcf5
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2022-11-09 20:44:48 -0500

    BuiltInFunctionNode constructor edit

--- src/BuiltInFunctionNode.java
+++ src/BuiltInFunctionNode.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 
 public abstract class BuiltInFunctionNode extends CallableNode {
-    private String name;
-    private ArrayList arguments;
     private boolean varadic;
+    private ArrayList<VariableNode> arguments;
+    private String name;
 
+    public BuiltInFunctionNode(String name, ArrayList<VariableNode> arguments) {
+        super(name, arguments);
+    }
 
     public BuiltInFunctionNode(String name, ArrayList<VariableNode> arguments, boolean varadic) {
-        super(name, arguments, varadic);
-        this.name = name;
+        super(name, arguments);
         this.arguments = arguments;
         this.varadic = varadic;
     }
+
 
     //Execute will take a collection of InterpreterDataType objects. Why? Well, when the interpreter finds a call to “read”, for example,
     // it has to be able to call your Java code.
