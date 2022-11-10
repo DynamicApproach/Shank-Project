236e6c1f145fe20b7870a8640ae21dde73dc9a70
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2022-11-09 20:44:02 -0500

    While -> Under Builtin Add execute

--- src/WhileNode.java
+++ src/WhileNode.java
@@ -1,13 +1,23 @@
 import java.util.ArrayList;
 
-public class WhileNode extends Node {
+public class WhileNode extends BuiltInFunctionNode {
     //While (booleanExpression and collection of statementNodes)
     private Node booleanExpression;
     private ArrayList<StatementNode> statementNodes;
 
     public WhileNode(Node booleanExpression, ArrayList<StatementNode> statementNodes) {
+        super("while", null, false);
         this.booleanExpression = booleanExpression;
         this.statementNodes = statementNodes;
+    }
+
+    public WhileNode(String name, ArrayList<VariableNode> arguments, boolean varadic) {
+        super(name, arguments, varadic);
+    }
+
+    @Override
+    public void execute(ArrayList<InterpreterDataType> arguments) throws Exception {
+        // TODO: finish
     }
 
     @Override
