50c7e34abbfb14df8a1c5c07cdef7f3b17eedd84
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2022-11-09 20:41:49 -0500

    Repeat -> BuiltInFunctionNode

--- src/RepeatNode.java
+++ src/RepeatNode.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
-public class RepeatNode extends Node {
+public class RepeatNode extends BuiltInFunctionNode {
     //Repeat (booleanExpression and collection of statementNodes)
     private Node booleanExpression;
     private ArrayList<Node> statementNodes;
 
     public RepeatNode(Node booleanExpression, ArrayList<Node> statementNodes) {
+        super("repeat", null, false);
         this.booleanExpression = booleanExpression;
         this.statementNodes = statementNodes;
     }
@@ -13,5 +14,10 @@ public class RepeatNode extends Node {
     @Override
     public String toString() {
         return "Repeat: " + statementNodes.toString() + " Until: " + booleanExpression.toString();
+    }
+
+    @Override
+    public void execute(ArrayList<InterpreterDataType> arguments) throws Exception {
+
     }
 }
